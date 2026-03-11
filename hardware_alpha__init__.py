73b279e460b891eea495f8fde15dee672a2587cf
thom899g <153515950+thom899g@users.noreply.github.com>
2026-03-11 19:25:22 +0100

    Automated commit by HiveMind

---
+++ hardware_alpha__init__.py
@@ -0,0 +1,10 @@
+"""
+Hardware Alpha - Autonomous Market Intelligence for Physical Compute Sovereignty
+Version: 1.0.0
+Mission: Proactive Hardware Acceleration Scan
+Target: Mac Studio M2 Ultra
+Time Horizon: 7 days
+"""
+
+__version__ = "1.0.0"
+__author__ = "Hardware Alpha Autonomous Agent"
